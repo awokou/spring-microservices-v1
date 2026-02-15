384e04a929bdc780a245245c8ffe580f4986e6d3
awokou <kawokou122@gmail.com>
2026-02-15 18:33:23 +0100

    Initial project

--- common-lib/src/main/java/com/server/common/lib/entity/Student.java
+++ common-lib/src/main/java/com/server/common/lib/entity/Student.java
@@ -1,6 +1,5 @@
 package com.server.common.lib.entity;
 
-import jakarta.persistence.Column;
 import lombok.*;
 
 @Data
@@ -8,12 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 public class Student {
 
-    @Column(name = "first_name", nullable = false)
     private String firstName;
-
-    @Column(name = "last_name", nullable = false)
     private String lastName;
-
-    @Column(name = "email", nullable = false, unique = true)
     private String email;
 }
